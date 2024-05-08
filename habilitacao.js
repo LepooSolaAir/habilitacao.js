6ad4c4b29f4b15c4d9508ceb01131df64d2ced12
Leonardo <leonardo.monegatto.santos@escola.pr.gov.br>
2024-05-08 09:07:50 -0300

    commitando habilitacao.js

---
+++ habilitacao.js
@@ -0,0 +1,10 @@
+let idade = prompt("Digite sua idade: ");
+ idade = parseInt(idade);
+
+if (idade > 18) {
+    alert("Pode tirar a habilitação");
+} else if (idade === 18) {
+    alert("Pode tirar a habilitação");
+} else {
+    alert("Não pode tirar a habilitação");
+}
